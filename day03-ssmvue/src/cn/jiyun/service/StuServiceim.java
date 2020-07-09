dcd440723a43d1577feb1ceb6df82be28f658e5f
WangFei768 <1746864565@qq.com>
2020-07-09 11:27:33 +0800

    提交day03-ssmvue

---
+++ day03-ssmvue/src/cn/jiyun/service/StuServiceim.java
@@ -0,0 +1,21 @@
+package cn.jiyun.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cn.jiyun.mapper.StuMapper;
+import cn.jiyun.pojo.QueryVo;
+@Service
+@Transactional
+public class StuServiceim implements StuService {
+@Autowired
+private StuMapper p;
+	public List<QueryVo> show() {
+		// TODO Auto-generated method stub
+		return p.show();
+	}
+
+}
